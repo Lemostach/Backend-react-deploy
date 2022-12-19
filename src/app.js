98f155d668eaad92320738adafae898a719d582e
Marcelo Velasco <marceluno10@gmail.com>
2022-12-19 17:33:00 +0100

    prueba

---
+++ src/app.js
@@ -0,0 +1,23 @@
+//APP
+
+const winston = require('winston')
+const express = require('express')
+const path = require('path');
+
+
+require('dotenv').config()
+
+const app = express();
+
+require('./startup/logging')()
+require('./startup/routes')(app)
+require('./startup/db')()
+
+//ARCHIVOS ESTATICOS --- (NO HE CONSEGUIDO QUE CARGUEN DESDE "src/startup/routes.js")
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+//PUERTO DE ESCUCHA
+
+const port = process.env.PORT || 3000
+app.listen(port, () => winston.info(`SERVIDOR CONECTADO EN: http://localhost:${port}`))
